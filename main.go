5188a5304a827bb2d4875562d24b356b55c08ad7
Erik Villegas <erdvillegas@gmail.com>
2020-04-24 00:05:02 -0500

    Agregando soporte para Docker

--- main.go
+++ main.go
@@ -4,15 +4,19 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
 
 func homeLink(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Nueva peticion IP = %v", r.RemoteAddr)
 	fmt.Fprintf(w, "Welcome home!")
 }
 
 func main() {
+	mensaje := os.Args[0]
+	fmt.Printf("Ejecutando desde :%s\n", mensaje)
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homeLink)
 	log.Fatal(http.ListenAndServe(":8080", router))
